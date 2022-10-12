aaf05fafd2ba92ca6535e0ddb94c00bb58cdd7d7
williamthome <willegp88@gmail.com>
2022-10-12 16:49:02 -0300

    feat: todos context

---
+++ test/support/fixtures/todos_fixtures.ex
@@ -0,0 +1,21 @@
+defmodule TodoApp.TodosFixtures do
+  @moduledoc """
+  This module defines test helpers for creating
+  entities via the `TodoApp.Todos` context.
+  """
+
+  @doc """
+  Generate a todo.
+  """
+  def todo_fixture(attrs \\ %{}) do
+    {:ok, todo} =
+      attrs
+      |> Enum.into(%{
+        done: true,
+        title: "some title"
+      })
+      |> TodoApp.Todos.create_todo()
+
+    todo
+  end
+end
