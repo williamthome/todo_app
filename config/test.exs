0e4531cebc8c66c718da5c65705119e5eae40f6b
williamthome <willegp88@gmail.com>
2022-10-12 16:42:18 -0300

    chore: init project

---
+++ config/test.exs
@@ -0,0 +1,30 @@
+import Config
+
+# Configure your database
+#
+# The MIX_TEST_PARTITION environment variable can be used
+# to provide built-in test partitioning in CI environment.
+# Run `mix help test` for more information.
+config :todo_app, TodoApp.Repo,
+  username: "postgres",
+  password: "postgres",
+  hostname: "localhost",
+  database: "todo_app_test#{System.get_env("MIX_TEST_PARTITION")}",
+  pool: Ecto.Adapters.SQL.Sandbox,
+  pool_size: 10
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :todo_app, TodoAppWeb.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "e6QcneWrPTSPJyde6OaHBGJTC50scMnzy7TehTPfdcjUw6H8u3KLiXKmeEDchHRd",
+  server: false
+
+# In test we don't send emails.
+config :todo_app, TodoApp.Mailer, adapter: Swoosh.Adapters.Test
+
+# Print only warnings and errors during test
+config :logger, level: :warn
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
